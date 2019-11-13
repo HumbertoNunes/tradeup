04d3d484f92f39e3ca982008b074e14f98e650b7
Humberto.Souza <humberto.nts@gmail.com>
2019-11-13 11:31:50 -0400

    Adicionando teste de atualização do valor

--- tests/Unit/RefundTest.php
+++ tests/Unit/RefundTest.php
@@ -10,15 +10,35 @@ use App\Refund;
 class RefundTest extends TestCase
 {
     /**
+     * @test
+     *
+     * Trying to change the refund's value before approve.
+     *
+     * @return void
+     */
+    public function update_value_before_approved()
+    {
+        $refund = factory(Refund::class)->make();
+
+        $value = 200;
+
+        $refund->rectify($value);
+
+        $this->assertEquals($refund->value, $value);
+    }
+
+    /**
+     * @test
+     *
      * Trying to change the refund's value after approve.
      *
      * @return void
      */
-    public function test_update_value_after_approved()
+    public function update_value_after_approved()
     {
         $refund = factory(Refund::class)->make();
 
-    	$value = 200;
+        $value = 200;
 
         $refund->approve();
 
