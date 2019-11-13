a8ca6ab9e7ad50da671ca972bdcba714697218eb
Humberto.Souza <humberto.nts@gmail.com>
2019-11-13 11:22:13 -0400

    Tornando o atributo 'identification' único

--- database/migrations/2019_11_08_180513_create_people_table.php
+++ database/migrations/2019_11_08_180513_create_people_table.php
@@ -16,7 +16,7 @@ class CreatePeopleTable extends Migration
         Schema::create('people', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('identification');
+            $table->string('identification')->unique();
             $table->string('jobRole');
             $table->timestampTz('createdAt');
         });
